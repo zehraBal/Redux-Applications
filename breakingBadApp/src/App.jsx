f36af264bb6d7dacde391a43e9c176f71a0a634c
zehraBal <fz.bal00@gmail.com>
2025-07-05 01:19:40 +0300

    updated

--- breakingBadApp/src/App.jsx
+++ breakingBadApp/src/App.jsx
@@ -5,16 +5,18 @@ import Details from "./components/Details";
 import Header from "./components/Header";
 import Episodes from "./components/Episodes";
 import Locations from "./components/Locations";
+import Characters from "./components/Characters";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route exact path="/" Component={HomePage} />
-        <Route path="/details/:char_id" Component={Details} />
+        <Route exact path="/" Component={Characters} />
         <Route path="/episodes" Component={Episodes} />
-        <Route path="/locations" Component={Locations} />
+        {/*<Route path="/details/:char_id" Component={Details} />
+        
+        <Route path="/locations" Component={Locations} />*/}
       </Routes>
     </BrowserRouter>
   );
