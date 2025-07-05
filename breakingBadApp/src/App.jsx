3daa35e6d8c4ad02acacb19395e6a252441af83c
zehraBal <fz.bal00@gmail.com>
2025-07-05 21:05:11 +0300

    add all routes

--- breakingBadApp/src/App.jsx
+++ breakingBadApp/src/App.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import HomePage from "./components/HomePage";
-import Details from "./components/Details";
 import Header from "./components/Header";
 import Episodes from "./components/Episodes";
-import Locations from "./components/Locations";
 import Characters from "./components/Characters";
+import Details from "./components/Details";
+import Locations from "./components/Locations";
 
 function App() {
   return (
@@ -14,9 +13,8 @@ function App() {
       <Routes>
         <Route exact path="/" Component={Characters} />
         <Route path="/episodes" Component={Episodes} />
-        {/*<Route path="/details/:char_id" Component={Details} />
-        
-        <Route path="/locations" Component={Locations} />*/}
+        <Route path="/details/:char_id" Component={Details} />
+        <Route path="/locations" Component={Locations} />
       </Routes>
     </BrowserRouter>
   );
